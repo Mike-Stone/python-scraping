c329e1ad173f8ed8784a55a6b28db09f99610843
MikeStone <shihongyangabc@163.com>
2016-06-04 00:17:48 +0800

    Update 1-seleniumBasic.py

--- chapter10/1-seleniumBasic.py
+++ chapter10/1-seleniumBasic.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 import time
-
+#用PlantomJS浏览器加载
 driver = webdriver.PhantomJS(executable_path='')
 driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
 time.sleep(3)
+#静等三秒，期望JS已经加载完成
 print(driver.find_element_by_id("content").text)
 driver.close()
